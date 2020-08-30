3c8fc3615af5d4289c2ef07c0404751378da77e8
Sahil Mor <43886039+sahil-mor@users.noreply.github.com>
2020-08-31 02:38:16 +0530

    changing UI

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import {createAppContainer} from 'react-navigation'
-import {createStackNavigator, HeaderTitle} from 'react-navigation-stack'
+import {createStackNavigator} from 'react-navigation-stack'
 
 import Home from './screens/Home'
-import Movie from './screens/Movie'
 const mainNavigator = createStackNavigator({
   Home : { screen : Home },
-  Movie : { screen : Movie }
 },
  {
   defaultNavigationOptions :{
-    headerStyle : {
-      backgroundColor : "grey"
-    },
-    headerTitle : "Home of Movies",
-    headerTitleStyle : {
-      color : "white",
-      fontWeight : "bold"
-    },
-    headerTintColor : "white"
+    headerShown : false
   }
 }
 )
