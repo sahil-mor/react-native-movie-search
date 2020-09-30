cde100e93b1136a3d2be10cd46c4d7b566287ca5
zabhitak <zabhitak1292000@gmail.com>
2020-10-01 00:42:14 +0530

    DOCKER

--- plinko/score.js
+++
@@ -1,8 +0,0 @@
-function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
-}
-
-function runAnalysis() {
-  // Write code here to analyze stuff
-}
-
